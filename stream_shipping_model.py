3121f5c65d60bc5b3e10d1699d2c6582d66a506d
asyrofwajdi <asyrofwajdi@yahoo.com>
2024-07-26 03:09:56 +0800

    Add files via upload

--- stream_shipping_model.py
+++ stream_shipping_model.py
@@ -1,32 +1,47 @@
 import pickle
 import streamlit as st
+import os
 
-# Reading Model
-shipping_model = pickle.load(open('shipping_model.sav','rb'))
+# Ensure the required library is installed
+try:
+    from sklearn.ensemble import RandomForestClassifier
+except ModuleNotFoundError:
+    st.error("The scikit-learn library is not installed. Please install it by running 'pip install scikit-learn'.")
 
-# web title
-st.title('Shipping Logistic Prediction')
+# Check if the model file exists
+model_path = 'shipping_model.sav'
 
-Warehouse_block = st.text_input('Warehouse block')
-Mode_of_Shipment = st.text_input('Mode of Shipment')
-Customer_care_calls = st.text_input('Customer care call')
-Customer_rating = st.text_input('Customer Rating')
-Cost_of_the_Product = st.text_input('Cost of product')
-Prior_purchases = st.text_input('Prior purchase')
-Product_importance = st.text_input('Product important')
-Gender = st.text_input('Gender')
-Discount_offered = st.text_input('Discount Offered')
-Weight_in_gms = st.text_input('Weight in grams')
+if not os.path.exists(model_path):
+    st.error(f"Model file not found at {model_path}")
+else:
+    # Reading Model
+    with open(model_path, 'rb') as file:
+        shipping_model = pickle.load(file)
 
-# Code for prediction
-delivery = ''
-# Result
-if st.button('Delivery Prediction'):
-    preds = grid_search_dt.predict([[Warehouse_block, Mode_of_Shipment, Customer_care_calls, Customer_rating, Cost_of_the_Product, Prior_purchases, Product_importance, Gender, Discount_offered, Weight_in_gms]])
-    
-    if(preds[0] == 1):
-        delivery = 'Not reach on time'
-    else : 
-        delivery = 'Reach on time'
-    st.success(delivery)
+    # web title
+    st.title('Shipping Logistic Prediction')
+
+    # Input fields
+    Warehouse_block = st.text_input('Warehouse block')
+    Mode_of_Shipment = st.text_input('Mode of Shipment')
+    Customer_care_calls = st.number_input('Customer care calls', min_value=0, step=1)
+    Customer_rating = st.number_input('Customer Rating', min_value=1, max_value=5, step=1)
+    Cost_of_the_Product = st.number_input('Cost of product', min_value=0)
+    Prior_purchases = st.number_input('Prior purchases', min_value=0, step=1)
+    Product_importance = st.text_input('Product importance')
+    Gender = st.text_input('Gender')
+    Discount_offered = st.number_input('Discount Offered', min_value=0)
+    Weight_in_gms = st.number_input('Weight in grams', min_value=0)
+
+    # Code for prediction
+    delivery = ''
+    # Result
+    if st.button('Delivery Prediction'):
+        preds = shipping_model.predict([[Warehouse_block, Mode_of_Shipment, Customer_care_calls, Customer_rating, Cost_of_the_Product, Prior_purchases, Product_importance, Gender, Discount_offered, Weight_in_gms]])
+        
+        if(preds[0] == 1):
+            delivery = 'Not reach on time'
+        else:
+            delivery = 'Reach on time'
+        st.success(delivery)
 
